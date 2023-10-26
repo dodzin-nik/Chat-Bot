aaae0e888683564c3f6b7810a6a9325347257601
dodzin-nik <53540499+dodzin-nik@users.noreply.github.com>
2023-10-26 12:27:08 +0200

    Переработка дизайна

---
+++ bot.py
@@ -0,0 +1,23 @@
+from questions import q, default_answer
+from fuzzywuzzy import fuzz
+
+class ChatBot():
+
+    def Answer(self, user_text):
+        self.user_text = user_text
+
+        while True:
+
+
+            if self.user_text == "Соедините меня с оператором":
+                break
+            
+            if fuzz.partial_ratio(user_text, q.get(1)[0]) >= 70:
+                answer = q.get(1)[1]
+            elif fuzz.partial_ratio(user_text, q.get(2)[0]) >= 70:
+                answer = q.get(2)[1]
+            else:
+                answer = default_answer
+            
+            print(answer)
+            return answer
--- messages.py
+++ messages.py
@@ -1,68 +1,117 @@
 import flet as ft
-from fuzzywuzzy import fuzz
-from questions import q, default_answer
+from bot import ChatBot
 
-class ChatBot(ft.UserControl):
+class Message():
+    def __init__(self, user_name: str, text: str, message_type: str):
+        self.user_name = user_name
+        self.text = text
+        self.message_type = message_type
 
-    def build(self):
-        self.heading = ft.Text(value="Техподдержка", size=24)
-        self.text_input = ft.TextField(hint_text="Введите ваш вопрос...", expand=True, multiline=True)
-        self.output_column = ft.Column()
-        self.scroll = True
-        return ft.Column(
-            width=370,
-            controls=[
-                self.heading,
-                ft.Row(
-                    controls=[
-                        self.text_input,
-                        ft.ElevatedButton("отправить", height=60, style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=1)), on_click=self.button_clicked),
-                    ],
-                ),
-                self.output_column,
-            ],
-        )
-    
-    def button_clicked(self, event):
-        typization = self.text_input.value
-        if fuzz.partial_ratio(typization, q.get(1)[0]) >= 70:
-            answer = q.get(1)[1]
-        elif fuzz.partial_ratio(typization, q.get(2)[0]) >= 70:
-            answer = q.get(2)[1]
-        else:
-            answer = default_answer
-        self.output = answer
-        result = Output(self.output, self.text_input.value, self.removing)
-        self.output_column.controls.append(result)
-        self.text_input.value = ""
-        self.update()
-
-    def removing(self, result):
-        self.output_column.controls.remove(result)
-        self.update()
-
-class Output(ft.UserControl):
-    def __init__(self, myoutput, mytext_input, myoutput_delete):
+class ChatMessage(ft.Row):
+    def __init__(self, message: Message):
         super().__init__()
-        self.myoutput = myoutput 
-        self.mytext_input = mytext_input
-        self.myoutput_delete = myoutput_delete
+        self.vertical_alignment="start"
+        self.controls=[
+                ft.CircleAvatar(
+                    content=ft.Text(self.get_initials(message.user_name)),
+                    color=ft.colors.WHITE,
+                    bgcolor=self.get_avatar_color(message.user_name),
+                ),
+                ft.Column(
+                    [
+                        ft.Text(message.user_name, weight="bold"),
+                        ft.Text(message.text, selectable=True, width=500),
+                    ],
+                    tight=True,
+                    spacing=5,
+                ),
+            ]
 
-    def build(self):
-        self.output_display = ft.Text(value=self.myoutput, selectable=True)
-        self.delete_button = ft.IconButton(ft.icons.DELETE_OUTLINE_SHARP, on_click=self.delete)
-        self.input_display = ft.Container(ft.Text(value=self.mytext_input), bgcolor=ft.colors.BLUE_GREY_100, padding=10)
-        self.display_view = ft.Column(controls=[self.input_display, self.output_display, self.delete_button])
-        return self.display_view
+    def get_initials(self, user_name: str):
+        return user_name[:1].capitalize()
 
-    def delete(self, event):
-        self.myoutput_delete(self)
+    def get_avatar_color(self, user_name: str):
+        colors_lookup = [
+            ft.colors.CYAN,
+            ft.colors.GREEN,
+            ft.colors.INDIGO,
+            ft.colors.LIME,
+            ft.colors.TEAL,
+            ft.colors.YELLOW,
+        ]
+        return colors_lookup[hash(user_name) % len(colors_lookup)]
 
-def main(page):
-    page.scroll = True
-    page.window_width = 370
-    page.window_height = 440
-    chat = ChatBot()
-    page.add(chat)
+def main(page: ft.Page):
+    page.horizontal_alignment = "stretch"
+    page.title = "Техподдержка"
+    page.theme_mode = "light"
 
-ft.app(target=main, port=8080, view=ft.AppView.WEB_BROWSER)
+
+    chat_bot = ChatBot()
+
+
+    def send_message_click(e):
+        if new_message.value != "":
+            page.pubsub.send_all(Message(page.session.get("user_name"), new_message.value, message_type="chat_message"))
+            page.pubsub.send_all(Message(user_name="Косуля Маша", text="", message_type="login_message"))
+            answer = chat_bot.Answer(str(new_message.value))
+            page.pubsub.send_all(Message("Косуля Маша", str(answer).lstrip(), message_type="chat_message"))
+
+            new_message.value = ""
+            new_message.focus()
+            page.update()
+
+    def on_message(message: Message):
+        if message.message_type == "chat_message":
+            m = ChatMessage(message)
+        elif message.message_type == "login_message":
+            m = ft.Text(message.text, italic=True, color=ft.colors.BLACK45, size=12)
+        chat.controls.append(m)
+        page.update()
+
+    page.pubsub.subscribe(on_message)
+
+
+    chat = ft.ListView(
+        expand=True,
+        spacing=10,
+        auto_scroll=True,
+    )
+
+
+    new_message = ft.TextField(
+        hint_text="Введите ваш вопрос...",
+        autofocus=True,
+        shift_enter=True,
+        min_lines=1,
+        max_lines=5,
+        filled=True,
+        expand=True,
+        border_radius=20,
+        on_submit=send_message_click,
+        border_color=ft.colors.BLUE
+    )
+
+    page.add(
+        ft.Row([ft.Text("Косуля Маша", style="headlineLarge", color="blue")], alignment="center"),
+        ft.Container(
+            content=chat,
+            border=ft.border.all(2, ft.colors.BLUE),
+            border_radius=20,
+            padding=10,
+            expand=True,
+        ),
+        ft.Row(
+            [
+                new_message,
+                ft.IconButton(
+                    icon=ft.icons.SEND_ROUNDED,
+                    tooltip="Send message",
+                    on_click=send_message_click,
+                    icon_color=ft.colors.BLUE
+                ),
+            ]
+        ),
+    )
+
+ft.app(port=8080, target=main, view=ft.WEB_BROWSER)
